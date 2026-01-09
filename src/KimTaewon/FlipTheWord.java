382f15663510a3a12f50ce51d8e37fbbe76303ee
0802222 <0802222@naver.com>
2026-01-09 23:37:28 +0900

    (0109) FlipTheWord.java

---
+++ src/KimTaewon/FlipTheWord.java
@@ -0,0 +1,49 @@
+package KimTaewon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FlipTheWord {
+
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+
+        // 방법1. reverse()
+//        for (String x : str) {
+//            String tmp = new StringBuilder(x).reverse().toString();
+//            answer.add(tmp);
+//        }
+
+        // 방법2. 배열로 구현
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FlipTheWord T = new FlipTheWord();
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] str = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            str[i] = sc.next();
+        }
+
+        for (String x : T.solution(n, str)) {
+            System.out.println(x);
+        }
+    }
+}
