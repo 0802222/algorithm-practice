d723ed39436004a3588d94e3ef5ac3e7509516d1
0802222 <0802222@naver.com>
2026-01-16 23:10:15 +0900

    (0116) Password.java

---
+++ src/KimTaewon/Password.java
@@ -0,0 +1,30 @@
+package KimTaewon;
+
+import java.util.Scanner;
+
+public class Password {
+
+    public String solution(int n, String s) {
+        String answer = "";
+
+        for (int i = 0; i < n; i++) {
+            String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+
+            answer += (char) num;
+            s = s.substring(7);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Password T = new Password();
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+
+        System.out.println(T.solution(n, str));
+    }
+}
