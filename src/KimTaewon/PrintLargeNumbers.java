d57cb49dc1c1e3d0bb0ff50f7e40aadf1b777d27
0802222 <0802222@naver.com>
2026-01-17 17:10:16 +0900

    (0117) PrintLargeNumbers.java

---
+++ src/KimTaewon/PrintLargeNumbers.java
@@ -0,0 +1,36 @@
+package KimTaewon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PrintLargeNumbers {
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        answer.add(arr[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1]) {
+                answer.add(arr[i]);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PrintLargeNumbers T = new PrintLargeNumbers();
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
