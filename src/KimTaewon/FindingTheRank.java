7fad8da9b27270d267cef76676febaabbffc9115
0802222 <0802222@naver.com>
2026-01-25 21:09:15 +0900

    (0125) FindingTheRank.java

---
+++ src/KimTaewon/FindingTheRank.java
@@ -0,0 +1,37 @@
+package KimTaewon;
+
+import java.util.Scanner;
+
+public class FindingTheRank {
+
+    public int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > arr[i]) {
+                    cnt++;
+                }
+            }
+            answer[i] = cnt;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FindingTheRank T = new FindingTheRank();
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] += sc.nextInt();
+        }
+
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
